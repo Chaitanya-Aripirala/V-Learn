31b039e92c8a7c484761d36ca80b037860da535a
Aripirala Chaitanya <aripiralachaitanya.new@gmail.com>
2026-05-18 15:49:53 +0530

    chore: connect frontend to render backend URL

--- my-project/src/services/api.js
+++ my-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://v-learn-5n7j.onrender.com/api',
 });
 
 // Add a request interceptor
